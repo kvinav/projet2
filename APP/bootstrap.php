8a577375c350bca87fb61ebba65411e5a7aec0ca
kvinav <kavignonpro@gmail.com>
2017-11-30 17:25:54 +0100

    add html.twig files and autoload in composer.json

--- APP/bootstrap.php
+++ APP/bootstrap.php
@@ -8,6 +8,11 @@ date_default_timezone_set("Europe/Paris");
 require 'config.php';
 require '../MODEL/Autoloader.php';
 Autoloader::register();
+require '../vendor/autoload.php';
+    
+    $loader = new Twig_Loader_Filesystem('../VIEW'); // Dossier contenant les templates
+    $twig = new Twig_Environment($loader, array('cache' => false));
+
 
 
 $bdd = APP::getDatabase();
